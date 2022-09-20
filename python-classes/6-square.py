2147ef0556640caecde8fc0e6775051afee6e124
crasride <jose.dfernandez@yahoo.fr>
2022-09-20 14:38:16 +0200

    update

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,8 +40,8 @@ class Square:
         elif (value < 0):
             raise ValueError("size must be >= 0")
 
-        def area(self):
-            return (self.__size*self.__size)
+    def area(self):
+        return (self.__size*self.__size)
 
     def my_print(self):
         if self.__size == 0:
