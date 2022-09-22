2565656c45ffd8bb38d593eb61c449d202b45738
crasride <jose.dfernandez@yahoo.fr>
2022-09-22 16:17:53 +0200

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Prints a text with 2 new lines after each of these characters: ., ? and :
+-strip() returns a copy of the string with leading and trailing blank
+characters removed
+-split() or separate a string into parts
+-join() method returns a string in which the string elements of sequence
+have been joined by str separator.
+"""
+
+
+def text_indentation(text):
+    """ Prints a text with 2 new lines """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    else:
+        for doc in ".:?":
+            text = (doc + "\n\n").join(
+                    [line.strip(" ") for line in text.split(doc)])
+            print("{}".format(text), end="")
