778e41ed334d1bd0c880bf5233cc2f251850653d
crasride <jose.dfernandez@yahoo.fr>
2022-09-22 09:07:43 +0200

    Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python function that prints a square with the character "#"
+"""
+
+
+def print_square(size):
+    """ Print square with the character # """
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+    else:
+        for row in range(size):
+            for column in range(size):
+                print("#", end="")
+            print("")
