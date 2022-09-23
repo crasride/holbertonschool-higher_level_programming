91a2b2602bc5cbac7299bc233e1484f2534240f5
crasride <jose.dfernandez@yahoo.fr>
2022-09-23 10:15:11 +0200

    File Test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -15,6 +15,16 @@ class TestMaxInteger(unittest.TestCase):
         result = max_integer(list_negative)
         self.assertEqual(result, -1)
 
+    def test_integer(self):
+        """Test with list integer values return the max"""
+        list_integer = [2, 6, 1]
+        result = max_integer(list_integer)
+        self.assertEqual(result, 6)
+
+    def test_max_int_basic(self):
+        """ tests normal list of ints"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
 
 if __name__ == '__main__':
     unittest.main()
