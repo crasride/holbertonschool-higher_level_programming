bc5e05edd19df9c5b55352322a81f8aa4bfa4b88
crasride <jose.dfernandez@yahoo.fr>
2022-09-27 12:48:29 +0200

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Write a class MyList that inherits from list """
+
+
+class MyList(list):
+    
+    """
+    Class MyList is a sub class of list.
+    """
+    def print_sorted(self):
+        """
+        instance method prints the list sorted(ascending sort)
+        """
+        print(sorted(self))
