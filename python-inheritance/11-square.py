7cb0d28bd8dd792c3478daa061e821844aaa5655
crasride <jose.dfernandez@yahoo.fr>
2022-09-28 10:23:46 +0200

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Classe Rectangle inherits in BaseGeometry """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Private instance attribute size.
+    Inherits from Rectangle
+    Public method area()
+    """
+
+    def __init__(self, size):
+        """Initializes a Square / size of the square """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return str("[Square] {}/{}".format(self.__size, self.__size))
+
+    def area(self):
+        """ print area """
+        return self.__size ** 2
