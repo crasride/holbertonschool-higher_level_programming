f1552f3b26a78b2d09dde167e1cc3e1d2902f58f
crasride <jose.dfernandez@yahoo.fr>
2022-10-03 13:23:17 +0200

    First Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,11 +4,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """ 
-    Class Rectangle inherits from Base
-    """
+    """ Class Rectangle inherits from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
+        Private instance
         Itnitializes a Rectangle
         Args:
         __width -> width
@@ -21,3 +20,39 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """assign each arguments to right attribute"""
+        return self.__width
+
+    @property
+    def height(self):
+        """assign each arguments to right attribute"""
+        return self.__height
+
+    @property
+    def x(self):
+        """assign each arguments to left attribute"""
+        return self.__x
+
+    @property
+    def y(self):
+        """assign each arguments to left attribute"""
+        return self.__y
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
