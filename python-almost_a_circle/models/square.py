e366804656ec0ef77a0b65f0c6d2db2a9504b67b
crasride <jose.dfernandez@yahoo.fr>
2022-10-04 15:23:05 +0200

    Square update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,3 +26,20 @@ class Square(Rectangle):
             self.__class__.__name__, self.id, self.x, self.y,
             self.width)
         return str
+
+    def update(self, *args, **kwargs):
+        """
+        args is the list of arguments - no-keyworded arguments
+        1st argument should be the id attribute
+        2nd argument should be the size attribute
+        3rd argument should be the x attribute
+        4th argument should be the y attribute
+        """
+        l_attribute = ["id", "x", "y"]
+        for attribute, arg in zip(l_attribute, args):
+            setattr(self, attribute, arg)
+# setattr sets the value of the specified attribute of the specified object
+# zip eturns an iterator of tuples based on the iterable objects
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+# itemsreturns view object contains the key-value pairs of the dict,tuples,list
