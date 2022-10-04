523fe65a4c579c1fa87969c4ea3a4ad4c4f0425f
crasride <jose.dfernandez@yahoo.fr>
2022-10-04 14:59:50 +0200

    And now, the Square!

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Initializer square class """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ Returns size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for size """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """method should return """
+        str = "[{}] ({}) {}/{} - {}".format(
+            self.__class__.__name__, self.id, self.x, self.y,
+            self.width)
+        return str
