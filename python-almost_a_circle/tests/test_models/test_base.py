53e1fcc99d7b5784120ae250af9afb3d18770340
crasride <jose.dfernandez@yahoo.fr>
2022-10-05 10:55:58 +0200

    update

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -8,23 +8,23 @@ class TestBase(unittest.TestCase):
     """"""
     def test_to_json_string(self):
         """ Test to_json_string """
-        d = {'id': 1, 'x': 2, 'y': 3, 'width': 4, 'height': 5}
-        json_d = Base.to_json_string([d])
-        self.assertTrue(isinstance(d, dict))
-        self.assertTrue(isinstance(json_d, str))
+        dic = {'id': 1, 'x': 2, 'y': 3, 'width': 4, 'height': 5}
+        json_dic = Base.to_json_string([dic])
+        self.assertTrue(isinstance(dic, dict))
+        self.assertTrue(isinstance(json_dic, str))
         self.assertCountEqual(
-            json_d, '{["id": 1, "x": 2, "y": 3, "width": 4, "height": 5]}')
+            json_dic, '{["id": 1, "x": 2, "y": 3, "width": 4, "height": 5]}')
         json_d_1 = Base.to_json_string(None)
         self.assertEqual(json_d_1, "[]")
         err = ("to_json_string() missing 1 required positional argument: " +
                "'list_dictionaries'")
-        with self.assertRaises(TypeError) as e:
+        with self.assertRaises(TypeError) as i:
             Base.to_json_string()
-        self.assertEqual(err, str(e.exception))
+        self.assertEqual(err, str(i.exception))
         err = "to_json_string() takes 1 positional argument but 2 were given"
-        with self.assertRaises(TypeError) as e:
+        with self.assertRaises(TypeError) as i:
             Base.to_json_string([{1, 2}], [{3, 4}])
-        self.assertEqual(err, str(e.exception))
+        self.assertEqual(err, str(i.exception))
 
 if __name__ == '__main__':
     unittest.main()
