dbd75d3d1e0c21ee74700b67db72abcc158b0eaf
crasride <jose.dfernandez@yahoo.fr>
2022-10-05 10:00:40 +0200

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -58,3 +58,17 @@ class Base():
                 dummy = cls(1)
             dummy.update(**dictionary)
             return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        list classes instantiate from file of json string
+        return a list of instances the type of these instances depends on cls
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except:
+            return []
