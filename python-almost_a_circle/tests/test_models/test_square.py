504c3ab328faf7fa6e85344f2b4914ebd05a9be1
crasride <jose.dfernandez@yahoo.fr>
2022-10-05 15:04:10 +0200

    update

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -51,5 +51,22 @@ class TestSquare(unittest.TestCase):
         s = Square(1, 2)
         self.assertEqual(s.__str__(), '[Square] (1) 2/0 - 1')
 
-    if __name__ == "__main__":
-        unittest.main()
+    def test_update(self):
+        """test update()"""
+        s1 = Square(5)
+        s1.update(10)
+        self.assertEqual(s1.id, 10)
+        s1.update(x=12)
+        self.assertEqual(s1.x, 12)
+        s1.update(size=7, id=89, y=1)
+        self.assertEqual(s1.size, 7)
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.y, 1)
+        s1.update()
+        self.assertEqual(s1.size, 7)
+        self.assertEqual(s1.id, 89)
+        self.assertEqual(s1.y, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
