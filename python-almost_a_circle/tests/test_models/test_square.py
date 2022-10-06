935919eedc864e3aba7a98fadb29558171f7ca80
crasride <jose.dfernandez@yahoo.fr>
2022-10-06 12:45:41 +0200

    update

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -103,6 +103,14 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(s.x, 2)
         self.assertEqual(s.y, 3)
 
+    def test_to_dictionary(self):
+        """
+        Test of to_dictionary() in Square exists
+        """
+        dict = {'id': 7, 'size': 10, 'x': 9, 'y': 8}
+        s = Square(10, 9, 8, 7)
+        self.assertEqual(dict, s.to_dictionary())
+
 
 if __name__ == "__main__":
     unittest.main()
