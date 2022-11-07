85585997ae31b302092d5bec600807a98d4eac51
crasride <jose.dfernandez@yahoo.fr>
2022-11-07 14:01:18 +0100

    10. Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Import the database dump from hbtn_0d_tvshows
+-- (cat hbtn_0d_tvshows\.sql | sudo mysql -uroot -p hbtn_0d_tvshows)
+-- lists all shows contained in hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
