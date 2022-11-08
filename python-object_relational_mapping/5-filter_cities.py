b8cdf341e3e651531c6bc618185785c40a397f18
crasride <jose.dfernandez@yahoo.fr>
2022-11-08 17:16:15 +0100

    5. All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3],
                          port=3306)
     c = db.cursor()
-    c.execute("SELECT * FROM states,cities \
-              WHERE states.id=cities.state_id \
-              ORDER BY cities.id ASC")
+    c.execute("SELECT * FROM cities as c \
+              INNER JOIN states as s ON c.state_id = s.id \
+              ORDER BY c.id")
     print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
     c.close()
     db.close()
