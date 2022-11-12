591d736da0195d129bd5b8c4d9a14d7840a4c634
crasride <jose.dfernandez@yahoo.fr>
2022-11-12 18:47:09 +0100

    9. Contains a

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists all State objects that contain letter from the database hbtn_0e_6_usa
+You must use the module SQLAlchemy
+take 3 arguments
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # MySQL connection using mysqldb driver
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    s = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    for state in s:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
