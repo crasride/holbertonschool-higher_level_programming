6ca7db134ebe9117baa46010d2fe3568a10f13fe
crasride <jose.dfernandez@yahoo.fr>
2022-12-08 18:12:43 +0100

    6. Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// variable defined
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+// initialize variable
+let i = 0;
+
+while (i < text.length) {
+  console.log(text[i]);
+  i++;
+}
