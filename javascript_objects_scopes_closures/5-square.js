3771c26461a9971895ae6fd878d8402622260ada
crasride <jose.dfernandez@yahoo.fr>
2022-12-09 16:27:10 +0100

    5. Square #0

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// create class Square and inherits from Rectangle
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
+// exports class Square
+module.exports = Square;
