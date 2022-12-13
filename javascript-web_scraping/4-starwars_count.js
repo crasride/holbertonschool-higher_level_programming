eacd1880b78cb89c3f245ab76eb241a808dd443b
crasride <jose.dfernandez@yahoo.fr>
2022-12-13 13:59:19 +0100

    4. Star wars Wedge Antilles

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
-// import module AXIOS
-const axios = require('axios');
-// start the counter
-let count = 0;
-// response get from url
-axios.get(process.argv[2]).then(
-  res => {
-    for (const movies of res.data.results) {
-      for (const id of movies.characters) {
-        // includes() returns true if a string contains a specified string
-        if (id.includes(18)) {
-          count++;
-        }
-      }
-    }
-    // print count
-    console.log(count);
-  });
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    // Convert JSON TO Objects(for analysis string conversion)
+    const restconv = JSON.parse(body).results;
+    console.log(restconv.reduce((count, movie) => {
+      // method returns true if a string ends with a specified string
+      return (movie.characters.find((character) => character.endsWith('/18/')))
+        // The conditional (ternary) operator
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
