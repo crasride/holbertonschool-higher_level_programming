e0ef7d13d6b9887994299c452c077923d20febec
crasride <jose.dfernandez@yahoo.fr>
2022-12-13 10:03:20 +0100

    2. Status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// that display the status code of a GET request
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error == null) {
+    console.log('code: ' + response.statusCode);
+  }
+});
